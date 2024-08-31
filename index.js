31e00e1831c613708d6294f37ee9e26ff684d5b5
oussamabonnor1 <oussamabonnor@gmail.com>
2024-08-31 22:22:35 +0100

    update app

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-const { Server } = require('@tus/server')
-const { FileStore } = require('@tus/file-store')
+// const { Server } = require('@tus/server')
+// const { FileStore } = require('@tus/file-store')
 
-const host = '127.0.0.1'
-const port = 4000
-const server = new Server({
-    path: '/files',
-    datastore: new FileStore({ directory: './files' }),
+// const host = '127.0.0.1'
+// const port = 4000
+// const server = new Server({
+//     path: '/files',
+//     datastore: new FileStore({ directory: './files' }),
+// })
+// server.listen({ host, port })
+
+
+const app = express()
+app.get("/", (req, res, nex) => {
+    res.json({ app: app_name })
 })
 
-server.listen({ host, port })
+const server = http.createServer(app);
+server.listen(port, () => {
+
+    setInterval(() => {
+        console.log(`${app_name} : ${new Date().toISOString()}`);
+    }, 1000)
+});
+
